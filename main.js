9ba75f956d2b3874e27a69eea242bd4d82d7b504
sammy45356 <98224209+sammy45356@users.noreply.github.com>
2024-06-06 18:30:34 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+function preload(){
+    
+}
+function setup(){
+    Canvas = createCanvas(500, 400);
+    Canvas.center();
+   
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+}
+function draw(){
+   
+}
+function Take_Snap(){
+    save('mustache.png');
+}
